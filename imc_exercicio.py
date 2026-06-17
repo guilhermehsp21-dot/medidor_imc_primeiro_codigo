c91f9b22a165b4deccbd04370adc96f0d59c2188
Guilherme Henrique Prado <guilhermehsp21@gmail.com>
2026-06-17 18:39:04 -0300

    Add files via upload

---
+++ imc_exercicio.py
@@ -0,0 +1,7 @@
+nome = "Guilherme"
+peso = 72
+altura = 1.68
+imc = peso / altura ** 2
+
+print(nome, "tem", altura, "de altura e pesa", peso, "quilos. Seu IMC é:", imc)
+print (f"{nome} tem {altura} de altura e pesa {peso} quilos, seu imc é:{imc:.2f}")
